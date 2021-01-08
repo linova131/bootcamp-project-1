f047a0e794d686ef003fe1d41dc197734d1f4648
Liz Novaski <lnovaski@travelers.com>
2021-01-08 14:25:19 -0600

    Friday Afternoon Update
    Stable save before fixing the randomization issue

--- js/quotes.js
+++ js/quotes.js
@@ -2,42 +2,50 @@ const quotes = [
   {verbage:`Some prices are just too high, no matter how much you may want the prize. The one thing you can't trade for your heart's desire is your heart.`,
    source:`Lois McMaster Bujold`, 
    citation:`Memory`,
-   year:`1996`},
+   year:`1996`,
+   id: 1},
 
    {verbage:`Experience suggests it doesn't matter so much how you got here, as what you do after you arrive.`,
    source:`Lois McMaster Bujold`, 
    citation:`Barrayar`,
-   year:`1991`},
+   year:`1991`,
+   id: 2},
   
   {verbage:`I'm going up to my room now, where I may die.`,
    source:`Diana Wynne Jones`,
    citation:`Howl's Moving Castle`,
-   year:`1986`},
+   year:`1986`,
+   id: 3},
 
    {verbage:`Being a hero means ignoring how silly you feel.`,
     source:`Diana Wynne Jones`,
     citation:`Fire and Hemlock`,
-    year:`1984`},
+    year:`1984`,
+    id: 4},
   
   {verbage:`Lymond's behaviour, as always, went to the limits of polite usage and then hurtled off into space.`,
    source:`Dorothy Dunnett`,
    citation:`Queens' Play`,
-   year:`1964`},
+   year:`1964`,
+   id: 5},
   
    {verbage:`Coming back to where you started is not the same as never leaving.`,
    source:`Terry Pratchett`,
    citation:`A Hat Full of Sky`,
-   year:`2004`},
+   year:`2004`,
+   id: 6},
 
    {verbage:`It is said that your life flashes before your eyes just before you die. That is true, it's called Life.`,
    source:`Terry Pratchett`,
    citation:`The Last Continent`,
-   year:`1998`},
+   year:`1998`,
+   id: 7},
 
    {verbage:`Steal five dollars and you're a common thief. Steal thousands and you're either the government or a hero.`,
     source: `Terry Pratchett`,
     citation:`Going Postal`,
-    year:`2004`},
+    year:`2004`,
+    id: 8},
 
 
 ];
--- js/script.js
+++ js/script.js
@@ -18,14 +18,44 @@ project 1 - A Random Quote Generator
 /***
  * `printQuote` function
 ***/
+const bgcolors = ['#5F0F40', '#9A031E','#FB8B24','#E36414','#0F4C5C'];
+let usedQuotes = [];
 
 function getRandomQuote() {
   let quoteNum = Math.floor(Math.random()*quotes.length);
   return quotes[quoteNum];
  }
+ 
+ function getColor() {
+  let colorNum = Math.floor(Math.random() *bgcolors.length );
+  let color = bgcolors[colorNum];
+  return color; 
+ }
 
- function printQuote() {
+
+function printQuote() {
+  //let quoteID;
   let quotePick = getRandomQuote();
+  let quoteID = quotePick.id;
+/*
+  for (;usedQuotes.includes(quoteID);) {
+    letquotePick = getRandomQuote();
+    let quoteID = quotePick.id;
+  };
+
+
+  */
+  //if (usedQuotes.includes(quoteID)) {
+   // console.log(usedQuotes.includes(quoteID));
+  //};
+  usedQuotes.push(quoteID);
+  
+
+
+  if (usedQuotes.length === quotes.length){
+    usedQuotes = [];
+  };
+  console.log(usedQuotes.length);
   let quoteWords = quotePick.verbage;
   let quoteAuth = quotePick.source;
   let quoteBook = quotePick.citation;
@@ -34,14 +64,12 @@ function getRandomQuote() {
   <p class="quote">${quoteWords}</p>
   <p class="source">${quoteAuth}<span class="citation">${quoteBook}</span><span class="year">${quoteYear}</span></p>`;
   document.querySelector('#quote-box').innerHTML = quoteHTML;
+  document.body.style.background = getColor();
   return quoteHTML;
  }
 
 
-
 document.querySelector('#quote-box').innerHTML = printQuote();
-
-
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
